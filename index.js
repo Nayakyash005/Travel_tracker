74870c3f83db44cc701d78b8761d041ef20ce565
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-02-02 20:28:38 +0530

    update

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ let myarr = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+let currId= 1;
+let users = [
+  {id:1,name:"yash",color:"teal"},
+  {id:2,name:"person2",color:"green"}, 
+  {id:3,name:"Tisra Aadmi",color:"yellow"}, 
+]
 app.get("/countries",async (req,res)=>{
   const result = await db.query("SELECT * FROM countries");
   let arr1= result.rows.map(ele => (ele.country_name));
@@ -25,8 +31,8 @@ app.get("/countries",async (req,res)=>{
 })
 
 async function fun() {
-  const ans1 = await db.query("SELECT country_code FROM visited_country");
-  // console.log(ans1);
+  const ans1 = await db.query("SELECT country_code FROM visited_country JOIN users ON visited_country.users_id = users.id WHERE users_id = $1;",[currId]);
+   console.log("first function ",ans1.rows);
   let arr = [];
   ans1.rows.forEach(ele => {
     arr.push(ele.country_code);
@@ -35,14 +41,28 @@ async function fun() {
   return arr;
 };
 
+async function fun2() {
+  const val1 = await db.query("SELECT * FROM users");
+   users = val1.rows;
+   
+   const val = users.find((user)=> user.id == currId);
+   console.log("inside fun", val);
+   return val;
+}
+
 app.get("/", async (req, res) => {
   //Write your code here.
 
   try {
     const arr = await fun();
+    const currUser = await fun2();
+    console.log(arr,"abc");
+    console.log("xyz",currUser.color)
     res.render("index.ejs", {
       countries: arr,
       total: arr.length,
+      users: users,
+      color: currUser.color
     })
   } catch (error) {
     console.log(error.message);
@@ -53,11 +73,12 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   try {
     const c_name = req.body.country;
+    const curr_member = await fun2();
    console.log(c_name);
     const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [c_name]);
     const ans = result.rows[0]; //  because the result we are getting is in the from of the array so we are converting into the object
     console.log(ans.country_code);
-    await db.query("INSERT INTO visited_country(country_code) VALUES($1)", [ans.country_code]);
+    await db.query("INSERT INTO visited_country(country_code,users_id) VALUES($1,$2)", [ans.country_code,curr_member.id]);
 
     res.redirect("/");
   } catch (error) {
@@ -92,6 +113,43 @@ app.get("/add/:id", async (req, res) => {
   }
 });
 
+
+app.post("/user",(req,res)=>{
+ try{
+   if(req.body.add === "new"){
+    console.log("succesfully got to the new page");
+    res.render("new0.ejs");
+   }else{
+    currId = req.body.user;
+    console.log("hello"+currId);  
+    res.redirect("/");
+   }
+
+ }catch{
+  console.log(error.message);
+  res.sendStatus(404).json("there is problem at the srerver end");
+ }
+})
+
+
+app.post("/new",async (req,res)=>{
+try{
+  const var2 = req.body.name; 
+  const var1 = req.body.color;
+  console.log(var1 + "and cloor is : " + var2);
+  const arr = await db.query("INSERT INTO users(name, color) VALUES($1, $2) RETURNING* ;",
+  [var2,var1]
+  );
+     currId = result.rows[0].id;
+    console.log(currId);
+     res.redirect("/");
+  
+}catch{
+  console.log(error.message);
+  res.sendStatus(400).json("there is error at our server :(");
+}
+});
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port} `);
 });
